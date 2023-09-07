8ed056fab9f97269caad4926b8852201878b42a5
mohammed-Alijl <moh.wa.2016@gmail.com>
2023-09-07 19:17:33 +0300

    fix: delete cascadeOnDelete

--- database/migrations/2023_09_07_160108_create_categories_table.php
+++ database/migrations/2023_09_07_160108_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('category_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnUpdate();
             $table->timestamps();
         });
     }
