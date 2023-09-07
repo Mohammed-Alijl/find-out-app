ba09771798af0740065589405c93f0db10cb01b2
mohammed-Alijl <moh.wa.2016@gmail.com>
2023-09-07 19:16:31 +0300

    feat: make the category type repository

---
+++ app/Repositories/CategoryTypeRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\BasicRepositoryInterface;
+use App\Models\CategoryType;
+
+class CategoryTypeRepository implements BasicRepositoryInterface
+{
+    public function getAll()
+    {
+        return CategoryType::get();
+    }
+
+    public function find($id)
+    {
+        return CategoryType::findOrFail($id);
+    }
+
+    public function create($request)
+    {
+        $type = new CategoryType();
+        $type->setTranslation('name','ar',$request->name_ar);
+        $type->setTranslation('name','en',$request->name_en);
+        $type->save();
+    }
+
+    public function update($request, $id)
+    {
+        $type = CategoryType::findOrFail($id);
+        $type->setTranslation('name','ar',$request->name_ar);
+        $type->setTranslation('name','en',$request->name_en);
+        $type->zone_id = $request->zone_id;
+        $type->save();
+    }
+
+    public function delete($id)
+    {
+        $zone = CategoryType::findOrFail($id);
+        $zone->delete();
+    }
+
+}
