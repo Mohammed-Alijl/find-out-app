aad0b03385e36ce188e91780c10ecdb784eb1692
mohammed-Alijl <moh.wa.2016@gmail.com>
2023-09-08 20:11:47 +0300

    feat: make the customer repository

---
+++ app/Repositories/CustomerRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\BasicRepositoryInterface;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class CustomerRepository implements BasicRepositoryInterface
+{
+    public function getAll()
+    {
+        return User::get();
+    }
+
+    public function find($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    public function create($request)
+    {
+        $customer = new User();
+        $customer->name = $request->name;
+        $customer->email = $request->email;
+        $customer->mobile_number = $request->mobile_number;
+        $customer->zone_id = $request->zone_id;
+        $customer->city_id = $request->city_id;
+        $customer->platform = $request->platform;
+        $customer->password = Hash::make($request->password);
+        $customer->save();
+    }
+
+    public function update($request, $id)
+    {
+        $customer = User::findOrFail($id);
+        $customer->name = $request->name;
+        $customer->email = $request->email;
+        $customer->mobile_number = $request->mobile_number;
+        $customer->zone_id = $request->zone_id;
+        $customer->city_id = $request->city_id;
+        $customer->platform = $request->platform;
+        if ($request->filled('password'))
+            $customer->password = Hash::make($request->password);
+        $customer->save();
+    }
+
+    public function delete($id)
+    {
+        $zone = User::findOrFail($id);
+        $zone->delete();
+    }
+}
