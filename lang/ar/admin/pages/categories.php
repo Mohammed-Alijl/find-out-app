5751c05b57bbfe3de32aba1f05b31eeeccb7e631
mohammed-Alijl <moh.wa.2016@gmail.com>
2023-09-08 19:50:47 +0300

    feat: translate the catgory crud to arabic

---
+++ lang/ar/admin/pages/categories.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'title' => 'الأقسام',
+    'add.category' => 'إضافة قسم',
+    'name' => 'الاسم',
+    'action' => 'الإجراء',
+    'name_ar' => 'الاسم بالعربية',
+    'name_en' => 'الاسم بالإنجليزية',
+    'name.invalid' => 'الرجاء إدخال اسم القسم',
+    'choose' => 'اختر...',
+    'type' => 'نوع القسم',
+    'close' => 'إغلاق',
+    'add' => 'إضافة',
+    'edit' => 'تعديل',
+    'ok' => 'موافق',
+    'edit.category' => 'تعديل القسم',
+    'edit.category.type' => 'تعديل نوع القسم',
+    'are.you.sure' => 'هل أنت متأكد؟',
+    'not.able.revert' => 'لن تتمكن من التراجع عن هذا!',
+    'delete' => 'حذف!',
+    'deleted' => 'تم الحذف!',
+    'category.add' => 'تمت إضافة القسم',
+    'category.edit' => 'تم تعديل القسم',
+    'category.cannot.delete' => 'القسم قيد الاستخدام',
+    'add.nested.category' => 'إضافة قسم فرعي',
+    'parent.category' => 'السم الرئيسي',
+    'category' => 'القسم',
+
+];
