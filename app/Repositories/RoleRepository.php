6a86db08a41b4ff0f2879bdba544a7f7c05244a8
mohammed-Alijl <moh.wa.2016@gmail.com>
2023-09-12 22:30:14 +0300

    feat: make the role repository

---
+++ app/Repositories/RoleRepository.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\BasicRepositoryInterface;
+use App\Models\Admin;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleRepository implements BasicRepositoryInterface
+{
+
+    protected $methods = ['view', 'add', 'edit', 'delete'];
+    protected $models = [
+        'category_type','category','zone','city','service','advertisement','advertisement_request',
+        'page','social','contact','user','customer'
+    ];
+
+    public function getAll()
+    {
+        return Role::orderBy('id', 'Asc')->get();
+    }
+
+    public function find($id): Admin
+    {
+        return Role::findOrFail($id);
+    }
+
+    public function create($request)
+    {
+        $role = new Role();
+        $role->name = $request->name;
+        $role->guard_name = 'admin';
+        $role->save();
+        $permissions = Permission::whereIn('name', $request->permission)->get();
+        $role->syncPermissions($permissions);
+    }
+
+    public function update($request, $id)
+    {
+        $role = Role::findOrFail($id);
+        $role->name = $request->name;
+        $role->save();
+        $permissions = Permission::whereIn('name', $request->permission)->get();
+        $role->syncPermissions($permissions);
+    }
+
+    public function delete($id)
+    {
+        $role = Role::findOrFail($id);
+        $role->delete();
+    }
+
+    public function getPermissions()
+    {
+        return Permission::get();
+    }
+
+    public function getMethods()
+    {
+        return $this->methods;
+    }
+
+    public function getModels()
+    {
+        return $this->models;
+    }
+
+}
