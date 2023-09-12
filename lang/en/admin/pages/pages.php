7f595b04e2e3eeefd9d9916ed29e5d67015086d2
mohammed-Alijl <moh.wa.2016@gmail.com>
2023-09-12 15:09:58 +0300

    feat: make the create page for the page crud

--- lang/en/admin/pages/pages.php
+++ lang/en/admin/pages/pages.php
@@ -6,4 +6,11 @@ return [
   'page.add' => 'Page Added',
   'page.edit' => 'Page Edited',
   'name' => 'Page Name',
+  'add.title' => 'Add New Page',
+  'add.description' => 'Add New Page From Here!',
+  'name_ar' => 'Name In Arabic',
+  'name_en' => 'Name In English',
+  'name.invalid' => 'Please Enter The Name',
+  'add' => 'Add',
+  'content' => 'Content',
 ];
