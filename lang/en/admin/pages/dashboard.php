0bc680c2da3e6808a8733cdcf663f77fa3999965
mohammed-Alijl <moh.wa.2016@gmail.com>
2023-09-13 11:39:09 +0300

    feat: make the landing page for dashboard

---
+++ lang/en/admin/pages/dashboard.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'welcome' => 'Welcome',
+    'dashboard' => 'To Dashboard'
+];
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,10 @@
 @section('page-header')
     <div class="row mb-2 mb-xl-3">
         <div class="col-auto d-none d-sm-block">
-            <h3><strong>Analytics</strong> / Dashboard</h3>
+            <h3>{{__('admin/pages/dashboard.welcome')}} <strong>{{auth('admin')->user()->name}}</strong> {{__('admin/pages/dashboard.dashboard')}} 👋</h3>
         </div>
     </div>
+@endsection
+@section('content')
+    <img src="https://images.unsplash.com/photo-1599658880436-c61792e70672?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" width="70%">
 @endsection
