d849bdf72cacb9848376b5a038edff4805d19d57
mohammed-Alijl <moh.wa.2016@gmail.com>
2023-09-13 12:16:15 +0300

    some edites in the dashbaord landing page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,5 +9,5 @@
     </div>
 @endsection
 @section('content')
-    <img src="https://images.unsplash.com/photo-1599658880436-c61792e70672?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" width="70%">
+    <img src="{{asset('img/photos/dashboard.png')}}" width="60%">
 @endsection
